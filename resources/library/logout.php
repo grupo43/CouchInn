f80513c49152683fb2bcb2f6d175fcdd8ddac013
Tano Abeleyra <tanoabeleyra@gmail.com>
2016-06-21 09:44:15 -0300

    Logout: Mejoras en la redirección

--- resources/library/logout.php
+++ resources/library/logout.php
@@ -2,9 +2,8 @@
 session_start();
 if (isset ($_GET['admin'])):
 	unset($_SESSION['admin']);
-	header("Location: /admin_login.php");
 elseif (isset ($_GET['user'])):
-	unset($_SESSION['user']);
-	header("Location: /");
+	unset($_SESSION['user']);	
 endif;
+header('Location: ' . $_SERVER['HTTP_REFERER']);
 ?>
