5556c0ff66399f26ea388a5c7fc7f3b12df6c42c
Tano Abeleyra <tanoabeleyra@gmail.com>
2016-07-03 20:06:41 -0300

    Realizar pregunta: Respetar diseño

--- js/add-question.js
+++ js/add-question.js
@@ -1,19 +1,25 @@
-var $questionForm = $('#add-question');
-$questionForm.submit(function($e) {
+$('#add-question').submit(function($e) {
 	$e.preventDefault();
 	var $questionList = $('#questions');
 	var couchID = $('input[name="couchID"]').val();
-	$.post('/resources/library/add_question.php', $questionForm.serialize(), function() {
+	$.post('/resources/library/add_question.php', $(this).serialize(), function() {
 		$.get('/resources/library/get_questions.php', {couchID: couchID}, function(questions) {
-			if (questions.length) {
-				$questionList.html('<ul>');
-				$.each(questions, function(index, question) {
-					$questionList.append('<li>' + question.question + '</li>');
-					if (question.answer.length) {
-						$questionList.append('<ul><li>' + question.answer + '</li></ul>');
-					}
-				});
-			}
+			$questionList.html('');
+			$.each(questions, function(index, question) {
+				$questionList.append(
+					'<li><i class="fa fa-comment" aria-hidden="true"></i> ' + question.question + '</li>'
+				);
+				if (question.answer.length) {
+					$questionList.append(
+						'<ul>' +
+							'<li><i class="fa fa-comments" aria-hidden="true"></i> ' + question.answer + '</li>' +
+						'</ul>'
+					);
+				}
+				$questionList.append(
+					'<br />'
+				);
+			});
 		});
 	});
 });
