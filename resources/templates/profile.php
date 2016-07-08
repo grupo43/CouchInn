d4decb5c46d752a3faa9ef43cf47f4d792c46946
Tano Abeleyra <tanoabeleyra@gmail.com>
2016-07-08 00:04:16 -0300

    Perfil de usuario: Invertir fecha de nacimiento

--- resources/templates/profile.php
+++ resources/templates/profile.php
@@ -36,7 +36,7 @@ endif;
 <div class="col-md-4">
 	<h4><strong>Nombre:</strong> <?php echo $user['name'] ?></h4>
 	<h4><strong>Email:</strong> <?php echo $user['email'] ?></h4>
-	<h4><strong>Nacimiento:</strong> <?php echo $user['birthdate'] ?></h4>
+	<h4><strong>Nacimiento:</strong> <?php echo implode('/', array_reverse(explode('-', $user['birthdate']))) ?></h4>
 	<h4><strong>Teléfono:</strong> <?php echo $user['phone_number'] ?></h4>
 	<br />
 	<button class="btn btn-primary" data-toggle="modal" data-target="#edit-user-modal">Modificar datos</button>
