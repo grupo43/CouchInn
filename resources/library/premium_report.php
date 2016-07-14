13d8003b396fbac5db8a35aad8d7d5f6b4500c35
Tano Abeleyra <tanoabeleyra@gmail.com>
2016-07-14 10:21:52 -0300

    Reporte de ventas premium: Arreglo en el algoritmo de fechas

--- resources/library/premium_report.php
+++ resources/library/premium_report.php
@@ -4,10 +4,11 @@ require_once 'functions.php';
 $from = "";
 $to = "";
 if (isset ($_GET['from']) && isset($_GET['to'])):
-	$from = implode('/', array_reverse(explode('/', $_GET['from'])));
-	$to = implode('/', array_reverse(explode('/', $_GET['to'])));
+	$from	= implode('-', array_reverse(explode('/', $_GET['from'])));
+	$to		= implode('-', array_reverse(explode('/', $_GET['to'])));
+	$to		= new DateTime($to);
+	$to		= $to->modify('+1 day')->format('Y-m-d');
 endif;
 
 header('Content-type: application/json');
 echo json_encode(getPremiumSales($from, $to));
-?>
