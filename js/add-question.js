407828db57e78891d48735d858777fcee7f8d2c2
Tano Abeleyra <tanoabeleyra@gmail.com>
2016-07-14 19:15:16 -0300

    Realizar pregunta: Restablecer el formulario después de enviarlo (2)

--- js/add-question.js
+++ js/add-question.js
@@ -1,10 +1,11 @@
 $('#add-question').submit(function($e) {
+	$form = $(this);
 	$e.preventDefault();
-	$.post('/resources/library/add_question.php', $(this).serialize(), function(user) {
+	$.post('/resources/library/add_question.php', $form.serialize(), function(user) {
 		var question = $('input[name="question"]').val();
 		$('#questions').prepend(
 			'<li><i class="fa fa-comment" aria-hidden="true"></i> <strong>' + user + ': </strong>' + question + '</li><br />'
 		);
-		$(this).trigger('reset');
+		$form.trigger('reset');
 	});
 });
